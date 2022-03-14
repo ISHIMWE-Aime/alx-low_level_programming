57d42cee83008d38147c2417d10bf4664a35c44c
ISHIMWE-Aime <fabricei645@gmail.com>
2022-03-14 14:38:27 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase amd omits q and e
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 'a';
+while (i <= 'z')
+{
+if (i != 'e' && i != 'q')
+{
+	putchar(i);
+}
+i++;
+}
+return (0);
+}
