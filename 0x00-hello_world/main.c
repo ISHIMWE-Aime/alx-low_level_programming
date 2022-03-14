903603f77192b56a636249ee0032549d1cb5b7aa
ISHIMWE-Aime <fabricei645@gmail.com>
2022-03-14 03:03:06 -0700

    c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main (void)
 {
-char str[] = "\"Programming is like building a multilingual puzzle";
-puts(str);
+puts("\"Programming is like building multilingual puzzle");
 return (0);
 }
-
