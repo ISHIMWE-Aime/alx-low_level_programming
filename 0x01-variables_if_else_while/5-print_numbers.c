cf80ef348012915e47c77f26293d796c49baa14b
ISHIMWE-Aime <fabricei645@gmail.com>
2022-03-14 14:59:13 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit in base 10 starting from 0
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (i = n; n < 10; n++)
+	{
+		printf("%d", n);
+		printf("\n");
+	}
+	return (0);
+}
