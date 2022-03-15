0bb0c3b1fb02c51747bdb18355cd67a6eeb967bc
ISHIMWE-Aime <fabricei645@gmail.com>
2022-03-15 13:50:12 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include <main.h>
+
 /**
- * main - prints_purtchar, followed by a new line
+ * main - prints _putchar, followed bybnew line
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,3 +17,4 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
+
