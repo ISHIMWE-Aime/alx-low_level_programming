4c3d79faaa3566b946a6574b6c50ba8423638416
ISHIMWE-Aime <fabricei645@gmail.com>
2022-04-05 05:13:41 +0200

    c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <main.c>
+
+/**
+ * main - print a number
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	int var = 9;
+	print("variable var: %d\n", var);
+	return (0);
+}
